33963c766a57514652c8cc184db356aa31f452cb
rnktlhr <rinakitluhur10@gmail.com>
2024-02-26 15:16:30 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int nPanjang, nLebar, nLuas;
+	cout << "Masukan Panjang= ";
+	cin >> nPanjang; 
+	cout << "Masukan Lebar = ";
+	cin >> nLebar;  
+	nLuas = nPanjang * nLebar;
+	cout << "luasnya adalah = " << nLuas << endl;
+}
