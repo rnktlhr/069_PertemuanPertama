09e6ee19835f745eb7b089647e9097311f46148b
rnktlhr <rinakitluhur10@gmail.com>
2024-02-26 15:39:21 +0700

    melengkapi algoritma

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 int main()
 {//begin
-
+	//numeric nPanjang, nlebar, nLuas
+	//display 'Masukan Panjang= '
+	//accept nPanjang
+	//display 'Masukan Lebar= '
+	//accept nLebar 
+	//compute nLuas = nPanjang * nLebar 
+	//display ' Luasnya adalah = '
 
 	int nPanjang, nLebar, nLuas;
 	cout << "Masukan Panjang = ";
